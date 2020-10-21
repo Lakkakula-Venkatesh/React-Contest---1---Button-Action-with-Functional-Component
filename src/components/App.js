bc03efd8f03c22ac3fa0761e617e56e52a1f84bd
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-10-21 21:00:40 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  const [para, setPara] = React.useState("");
+
+  const renderParagraph = function() {
+    setPara(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+  };
   return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
+    <>
+      <div id="main">// Do not alter the main div</div>
+      <button onClick={this.renderParagraph}></button>
+      {this.para}
+    </>
   );
 }
-
 
 export default App;
